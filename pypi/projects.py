16858c230dc9cb07fba25806f8e5919dad88aa78
werwty <bihan.zh@gmail.com>
2018-05-17 15:25:58 -0400

    add test for pagination pages

--- pypi/projects.py
+++ pypi/projects.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-import aiohttp
+import aiohttp, asyncio
 
-from typing import Dict
+from typing import Dict, List
+from urllib.parse import urlparse, parse_qs
 
 from . import defaults
 
 
-async def get(
+async def get_detail(
     name: str, url: str = defaults.API_URL, timeout: int = defaults.TIMEOUT
 ) -> Dict:
     url = f"{url}api/projects/{name}/"
@@ -23,3 +24,62 @@ async def since(
     async with aiohttp.ClientSession() as session:
         async with session.get(url, timeout=timeout) as resp:
             return await resp.json()
+
+
+async def get_page(
+    page: int, url: str = defaults.API_URL, timeout: int = defaults.TIMEOUT
+) -> List:
+    url = f"{url}api/projects/?page={page}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, timeout=timeout) as resp:
+            print("Checking Page %d " % page)
+            return await resp.json()
+
+
+async def get_pages(
+    num_pages: int, queue, url: str = defaults.API_URL, timeout: int = defaults.TIMEOUT
+) -> List:
+    queue = asyncio.Queue()
+
+    result_pages = []
+
+    # Create X Consumers
+    consumers = [
+        asyncio.ensure_future(_consume_page(queue, result_pages))
+        for i in range(defaults.CONSUMERS)
+    ]
+
+    await _produce_pages(queue, num_pages)
+
+    # wait until the consumer has processed all items
+    await queue.join()
+
+    # cancel all consumers after queue.join() is complete
+    for consumer in consumers:
+        consumer.cancel()
+    return result_pages
+
+
+async def get_last_page(
+    url: str = defaults.API_URL, timeout: int = defaults.TIMEOUT
+) -> int:
+
+    # Getting the resulting previous_page of a large pagination number
+    # to figure out how many total pages of data there are
+    i: int = 9999
+    page = await get_page(i)
+    last_page = parse_qs(urlparse(page["links"]["previous_page"]).query)["page"][0]
+    return int(last_page)
+
+
+async def _consume_page(queue, result_pages):
+    while True:
+        item = await queue.get()
+        result_pages.append(await item)
+        queue.task_done()
+
+
+async def _produce_pages(queue, n):
+    for i in range(1, n):
+        project = get_page(i)
+        await queue.put(project)
--- pypi/tests/base.py
+++ pypi/tests/base.py
@@ -4,6 +4,7 @@ import asyncio
 import unittest
 
 from .. import projects, journals
+from typing import Awaitable, List
 
 
 class TestJournals(unittest.TestCase):
@@ -18,7 +19,7 @@ class TestJournals(unittest.TestCase):
 
     def test_since(self) -> None:
         latest = self.loop.run_until_complete(journals.latest())
-        js = self.loop.run_until_complete(journals.since(latest['last_serial']))
+        js = self.loop.run_until_complete(journals.since(latest["last_serial"]))
         self.assertTrue(len(js), 5000)
 
 
@@ -26,13 +27,19 @@ class TestProjects(unittest.TestCase):
 
     def setUp(self):
         self.loop = asyncio.get_event_loop()
+        self.queue = asyncio.Queue(loop=self.loop, maxsize=10)
 
     # TODO: Mock aiohttp
-    def test_get(self) -> None:
-        pulp = self.loop.run_until_complete(projects.get("pulpcore"))
+    def test_get_detail(self) -> None:
+        pulp = self.loop.run_until_complete(projects.get_detail("pulpcore"))
         self.assertTrue("3.0.0a1.dev0" in pulp["releases"])
         self.assertEqual(275959, pulp["last_serial"])
 
     def test_since(self) -> None:
         since_projects = self.loop.run_until_complete(projects.since(275959)),
         self.assertTrue(since_projects)
+
+    def test_paginate(self) -> None:
+        last_page = self.loop.run_until_complete(projects.get_last_page())
+        pages = self.loop.run_until_complete(projects.get_pages(last_page, self.queue))
+        self.assertTrue(len(pages) > 200)
